f3a854ea4ed48b3638c1c10b931ff44dd95f29e9
Nirob Barman <niroborin111@gmail.com>
2023-08-26 11:33:02 +0600

    791A - Bear and Big Brother

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, i, count = 0;
+    scanf("%d %d", &a, &b);
+    while (a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
