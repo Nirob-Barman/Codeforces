1423c15cf4e9b0c75ac40d514210278dd3d976ed
Nirob Barman <niroborin111@gmail.com>
2023-08-27 08:25:11 +0600

    509A - Maximum in Table

--- A_Maximum_in_Table.cpp
+++ A_Maximum_in_Table.cpp
@@ -12,6 +12,7 @@ int main()
         table[i][0] = 1;
         table[0][i] = 1;
     }
+
     // for (int i = 0; i < n; i++)
     // {
     //     for (int j = 0; j < n; j++)
@@ -23,6 +24,7 @@ int main()
     //     }
     //     cout << endl;
     // }
+
     for (int i = 1; i < n; i++)
     {
         for (int j = 1; j < n; j++)
