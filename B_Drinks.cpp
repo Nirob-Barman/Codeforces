183d33c9f029ff9a22be85c6affd96582fca38e3
Nirob Barman <niroborin111@gmail.com>
2023-08-27 15:58:39 +0600

    200B - Drinks

---
+++ B_Drinks.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int percentages[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &percentages[i]);
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += percentages[i];
+    }
+
+    double averagePercentage = static_cast<double>(sum) / n;
+
+    printf("%.12lf\n", averagePercentage);
+
+    return 0;
+}
