f791240c8b0cc4cb417112dd6460af962f1fc5d6
Nirob-Barman <niroborin111@gmail.com>
2023-09-12 09:38:23 +0600

    J - Factorial

---
+++ J_Factorial.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long factorial(long long n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld\n", factorial(n));
+    return 0;
+}
