ac25e636ca935462957c02d9b780af176a0942de
Nirob-Barman <niroborin111@gmail.com>
2023-09-12 15:07:21 +0600

    219158B - Basic Data Types

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    long long int lli;
+    char c;
+    float f;
+    double d;
+    scanf("%d %lld %c %f %lf", &i, &lli, &c, &f, &d);
+    /// printf("%d %lld %c %f %lf\n",i,lli,c,f,d);
+    printf("%d\n%lld\n%c\n%0.2f\n%0.1f\n", i, lli, c, f, d);
+    return 0;
+}
