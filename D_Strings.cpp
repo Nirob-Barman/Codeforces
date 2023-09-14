b439683e925e83ac225c7605430ab77faa5fee0e
Nirob-Barman <niroborin111@gmail.com>
2023-09-14 08:24:48 +0600

    D - Strings

---
+++ D_Strings.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char a[11], b[11], ab[21] = "";
+    scanf("%s%s", a, b);
+
+    printf("%d %d\n", strlen(a), strlen(b));
+
+    strcat(ab, a);
+    strcat(ab, b);
+
+    printf("%s\n", ab);
+
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+    printf("%s %s\n", a, b);
+
+    return 0;
+}
